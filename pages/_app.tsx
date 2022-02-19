34ac0979bdd160495534b9c3827bd3e3cd51046c
Jason Kai <tkai@uwo.ca>
2022-02-19 15:54:16 -0500

    fix bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import { useEffect } from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
 
 import ScreenSizeProvider from '../services/screen-size/provider'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // @ts-ignore
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+
   return (
     <ScreenSizeProvider>
       <Component {...pageProps} />
