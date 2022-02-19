ed5b2f3159862ada4191be5e8ff2f6eaaa6db8b1
Jason Kai <tkai@uwo.ca>
2022-02-19 14:55:45 -0500

    add screensize manager

---
+++ services/screen-size/context.ts
@@ -0,0 +1,16 @@
+import { createContext, Dispatch, SetStateAction } from 'react';
+
+export interface ScreenSizeProviderType {
+    state: {
+        screenSize: number;
+    }
+    actions: {
+        setScreenSize: Dispatch<SetStateAction<number>> 
+    }
+}
+
+export type ScreenSizeContextType = ScreenSizeProviderType | undefined
+
+const ScreenSizeContext = createContext<ScreenSizeContextType>(undefined);
+
+export default ScreenSizeContext
