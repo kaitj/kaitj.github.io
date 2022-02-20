24d4829948f73803774033f6e6290f92fe3bf24f
Jason Kai <tkai@uwo.ca>
2022-02-20 12:21:21 -0500

    update navbar link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,7 +20,7 @@ export const NavBar = () => {
 
                 <div className="collapse navbar-collapse justify-content-end" id='navbarNav'>
                     <ul className="navbar-nav">
-                        <li className="nav-item"><a className="nav-link" href="#">about</a></li>
+                        <li className="nav-item"><a className="nav-link" href="/">about</a></li>
                         <li className="nav-item"><a className="nav-link" href="#">portfolio</a></li>
                         <li className="nav-item"><a className="nav-link" href="#">contact</a></li>
                     </ul>
