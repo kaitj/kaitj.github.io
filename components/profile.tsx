3e7745790a2f6bfdef1914e9c6b10af63edcb86a
Jason Kai <tkai@uwo.ca>
2022-02-20 13:12:50 -0500

    separate profile to a smaller grid

--- components/profile.tsx
+++ components/profile.tsx
@@ -12,17 +12,19 @@ export const Profile = () => {
                 </div>
 
                 <div className={['col-lg-6', styles.profile].join(' ')}>
-                    <p>Hi there and thanks for visiting! I am currently a PhD
-                        Candidate at Western University, studying the short-range connectivity
-                        in the human brain. My work uses MRI to reconstruct the connections
-                        of the brain, examining the reliability of identifying these connections
-                        and how they may be affected in association with disease.</p>
+                    <div className='col-lg-8'>
+                        <p>Hi there and thanks for visiting! I am currently a PhD
+                            Candidate at Western University, studying the short-range connectivity
+                            in the human brain. My work uses MRI to reconstruct the connections
+                            of the brain, examining the reliability of identifying these connections
+                            and how they may be affected in association with disease.</p>
 
-                    <p>To help accomplish my research goals, I use a variety of computational
-                        techniques to develop tools and perform analysis. I mainly program
-                        in Python, but am also familiar with other programming languages and
-                        have contributed to a number of different projects! You can check out
-                        some of the projects I have been involved in over on the portfolio page.</p>
+                        <p>To help accomplish my research goals, I use a variety of computational
+                            techniques to develop tools and perform analysis. I mainly program
+                            in Python, but am also familiar with other programming languages and
+                            have contributed to a number of different projects! You can check out
+                            some of the projects I have been involved in over on the portfolio page.</p>
+                    </div>
                 </div>
             </div>
         </main>
