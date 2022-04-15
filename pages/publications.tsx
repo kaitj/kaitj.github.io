f46fed6173fa090c09b69179171014153b4d69ca
Jason Kai <tkai@uwo.ca>
2022-04-15 15:06:38 -0400

    create under constructon pub page

---
+++ pages/publications.tsx
@@ -0,0 +1,35 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+import styles from '../styles/home.module.css'
+
+// Components
+import { NavBar } from '../components/navbar'
+import { Footer } from '../components/footer'
+
+const Publications: NextPage = () => {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Jason Kai</title>
+                <meta name="description" content="Portfolio website" />
+                <meta name="author" content="Jason Kai" />
+
+            </Head>
+
+            <NavBar />
+
+            <main className={styles.main}>
+                <div className="col-2"></div>
+
+                <div className="col-8">Under construction</div>
+
+                <div className="col-2"></div>
+            </main>
+
+            <Footer />
+        </div>
+    )
+}
+
+export default Publications
