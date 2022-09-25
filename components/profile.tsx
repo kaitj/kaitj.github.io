abd0fb3d2d2511bf28069b794a696acfbec40b85
Jason Kai <tkai@uwo.ca>
2022-09-24 22:31:25 -0400

    update profile

--- components/profile.tsx
+++ components/profile.tsx
@@ -3,9 +3,9 @@ export const Profile = () => {
         <div className="row">
             <h2>About</h2>
 
-            <p>I am a PhD Candidate in the <a href="https://khanlab.ca" target="_blank" rel="noreferrer">Khan Computational Imaging Lab</a> at Western University where I develop tools and pipelines to process and analyze the brain&apos;s short-change connections. Previously, I completed a Bachelor&apos;s of Engineering (Electrical and Biomedical) from McMaster University.</p>
+            <p>I earned my PhD in the <a href="https://khanlab.ca" target="_blank" rel="noreferrer">Khan Computational Imaging Lab</a> at Western University where I developed tools and pipelines towards understanding the brain&apos;s short-ranged connections. I also completed a Bachelor&apos;s of Engineering from McMaster University.</p>
 
-            <p>While I primarily use Python, I have experience with a variety of programming languages and software packages. Take a look at my <a href="https://github.com/kaitj" target="_blank" rel="noreferrer">Github</a> for some of the projects I have been involved in! </p>
+            <p>While I primarily code in Python, I have experience with a variety of programming languages and software packages. Take a look at my <a href="https://github.com/kaitj" target="_blank" rel="noreferrer">Github</a> for some of the projects I have been involved in! </p>
 
             <p>I am always happy to chat about any previous or potential projects - just send me a message through <a href="https://twitter.com/jasonkai" target="_blank" rel="noreferrer">Twitter</a> or by <a href="mailto:tkai@uwo.ca" target="_blank" rel="noreferrer" title="Email">email</a>.</p>
         </div>
