25968496dab0c600965c7134145b11982e98fcb4
Jason Kai <tkai@uwo.ca>
2022-10-13 22:05:15 -0400

    update about

--- components/profile.tsx
+++ components/profile.tsx
@@ -3,7 +3,7 @@ export const Profile = () => {
         <div className="row">
             <h2>About</h2>
 
-            <p>I earned my PhD in the <a href="https://khanlab.ca" target="_blank" rel="noreferrer">Khan Computational Imaging Lab</a> at Western University where I developed tools and pipelines towards understanding the brain&apos;s short-ranged connections. I also completed a Bachelor&apos;s of Engineering from McMaster University.</p>
+            <p>I earned my PhD in the <a href="https://khanlab.ca" target="_blank" rel="noreferrer">Khan Computational Imaging Lab</a> at Western University where I developed tools, reproducible pipelines and applied machine learning to large neuroimaging datasets to study the connections of the brain. I also previously completed a Bachelor&apos;s of Engineering from McMaster University.</p>
 
             <p>While I primarily code in Python, I have experience with a variety of programming languages and software packages. Take a look at my <a href="https://github.com/kaitj" target="_blank" rel="noreferrer">Github</a> for some of the projects I have been involved in! </p>
 
