6264251c42d48fa7ff02b2394785eb4cf259c176
Jason Kai <tkai@uwo.ca>
2023-03-25 23:05:29 -0400

    update about page

--- components/profile.tsx
+++ components/profile.tsx
@@ -3,11 +3,11 @@ export const Profile = () => {
         <div className="row">
             <h2>About</h2>
 
-            <p>I earned my PhD in the <a href="https://khanlab.ca" target="_blank" rel="noreferrer">Khan Computational Imaging Lab</a> at Western University where I developed tools, reproducible pipelines and applied machine learning to large neuroimaging datasets to study the connections of the brain. I also previously completed a Bachelor&apos;s of Engineering from McMaster University.</p>
+            <p>I earned my PhD in the <a href="https://khanlab.ca" target="_blank" rel="noreferrer">Khan Computational Imaging Lab</a> at the University of Western Ontario, where I engineered data processing pipelines for reproducible analysis and applied machine learning to large neuroimaging datasets in order to study the connections of the brain. Prior to joining the lab, I completed a Bachelor&apos;s of Engineering from McMaster University.</p>
 
-            <p>While I primarily code in Python, I have experience with a variety of programming languages and software packages. Take a look at my <a href="https://github.com/kaitj" target="_blank" rel="noreferrer">Github</a> for some of the projects I have been involved in! </p>
+            <p>I am experienced in numerous programming languages - take a look at my <a href="https://github.com/kaitj" target="_blank" rel="noreferrer">Github</a> for some of my contributions! </p>
 
-            <p>I am always happy to chat about any previous or potential projects - just send me a message through <a href="https://twitter.com/jasonkai" target="_blank" rel="noreferrer">Twitter</a> or by <a href="mailto:tkai@uwo.ca" target="_blank" rel="noreferrer" title="Email">email</a>.</p>
+            <p>I am always happy to chat about any projects or opportunities - schedule a coffee chat <a href="https://calendly.com/jasonkai/coffee-chat" target="_blank" rel="noreferrer">here</a>. Alternatively, feel free connect with me by <a href="mailto:tkai@uwo.ca" target="_blank" rel="noreferrer" title="Email">email</a>, <a href="https://twitter.com/jasonkai" target="_blank" rel="noreferrer">Twitter</a>, or <a href="https://linkedin.com/in/jasonkai" target="_blank" rel="noreferrer">LinkedIn</a>.</p>
         </div>
     );
 };
