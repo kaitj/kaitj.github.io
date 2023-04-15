d928e3ce1e2595640e24f8d05b5e4c92c1a5fc1b
Jason Kai <tkai@uwo.ca>
2023-04-15 11:37:30 -0400

    update calendar booking url

--- components/profile.tsx
+++ components/profile.tsx
@@ -1,13 +1,61 @@
 export const Profile = () => {
-    return (
-        <div className="row">
-            <h2>About</h2>
+  return (
+    <div className="row">
+      <h2>About</h2>
 
-            <p>I earned my PhD in the <a href="https://khanlab.ca" target="_blank" rel="noreferrer">Khan Computational Imaging Lab</a> at the University of Western Ontario, where I engineered data processing pipelines for reproducible analysis and applied machine learning to large neuroimaging datasets in order to study the connections of the brain. Prior to joining the lab, I completed a Bachelor&apos;s of Engineering from McMaster University.</p>
+      <p>
+        I earned my PhD in the{" "}
+        <a href="https://khanlab.ca" target="_blank" rel="noreferrer">
+          Khan Computational Imaging Lab
+        </a>{" "}
+        at the University of Western Ontario, where I engineered data processing
+        pipelines for reproducible analysis and applied machine learning to
+        large neuroimaging datasets in order to study the connections of the
+        brain. Prior to joining the lab, I completed a Bachelor&apos;s of
+        Engineering from McMaster University.
+      </p>
 
-            <p>I am experienced in numerous programming languages - take a look at my <a href="https://github.com/kaitj" target="_blank" rel="noreferrer">Github</a> for some of my contributions! </p>
+      <p>
+        I am experienced in numerous programming languages - take a look at my{" "}
+        <a href="https://github.com/kaitj" target="_blank" rel="noreferrer">
+          Github
+        </a>{" "}
+        for some of my contributions!{" "}
+      </p>
 
-            <p>I am always happy to chat about any projects or opportunities - schedule a coffee chat <a href="https://calendly.com/jasonkai/coffee-chat" target="_blank" rel="noreferrer">here</a>. Alternatively, feel free connect with me by <a href="mailto:tkai@uwo.ca" target="_blank" rel="noreferrer" title="Email">email</a>, <a href="https://twitter.com/jasonkai" target="_blank" rel="noreferrer">Twitter</a>, or <a href="https://linkedin.com/in/jasonkai" target="_blank" rel="noreferrer">LinkedIn</a>.</p>
-        </div>
-    );
+      <p>
+        I am always happy to chat about any projects or opportunities - schedule
+        a coffee chat{" "}
+        <a
+          href="https://cal.com/jasonkai/coffee-chat?duration=30"
+          target="_blank"
+          rel="noreferrer"
+        >
+          here
+        </a>
+        . Alternatively, feel free connect with me by{" "}
+        <a
+          href="mailto:tkai@uwo.ca"
+          target="_blank"
+          rel="noreferrer"
+          title="Email"
+        >
+          email
+        </a>
+        ,{" "}
+        <a href="https://twitter.com/jasonkai" target="_blank" rel="noreferrer">
+          Twitter
+        </a>
+        , or{" "}
+        <a
+          href="https://linkedin.com/in/jasonkai"
+          target="_blank"
+          rel="noreferrer"
+        >
+          LinkedIn
+        </a>
+        .
+      </p>
+    </div>
+  );
 };
