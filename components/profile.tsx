a2be5525e8408a34d7793bedb718322ebbd9a036
Jason Kai <tkai@uwo.ca>
2023-10-13 22:15:26 -0400

    update about page

--- components/profile.tsx
+++ components/profile.tsx
@@ -2,38 +2,42 @@ export const Profile = () => {
   return (
     <div className="row">
       <h2>About</h2>
-
       <p>
-        I earned my PhD in the{" "}
-        <a href="https://khanlab.ca" target="_blank" rel="noreferrer">
-          Khan Computational Imaging Lab
-        </a>{" "}
-        at the University of Western Ontario, where I engineered data processing
-        pipelines for reproducible analysis and applied machine learning to
-        large neuroimaging datasets in order to study the connections of the
-        brain. Prior to joining the lab, I completed a Bachelor&apos;s of
-        Engineering from McMaster University.
-      </p>
-
-      <p>
-        I am experienced in numerous programming languages - take a look at my{" "}
+        I&apos;m a software developer who enjoys working in cross-functional
+        teams and has a passion for leveraging diverse technologies to drive
+        meaningful advancements in the field of medical research. In my current
+        role, I develop reproducible data processing pipelines, build web
+        applications, and automate workflows to streamline the software
+        development and delivery processes. Check out my{" "}
         <a href="https://github.com/kaitj" target="_blank" rel="noreferrer">
           Github
         </a>{" "}
-        for some of my contributions!{" "}
+        for some of the projects I have worked on! Additionally, I contribute to
+        open-source projects and regularly co-organize hackathons to foster
+        collaboration.
       </p>
-
       <p>
-        I am always happy to chat about any projects or opportunities - schedule
-        a coffee chat{" "}
+        Previously, I earned my PhD in the{" "}
+        <a href="https://khanlab.ca" target="_blank" rel="noreferrer">
+          Khan Computational Imaging Lab
+        </a>{" "}
+        at the University of Western Ontario, where my research involved
+        engineering processing pipelines for reproducible analysis and applied
+        machine learning to large datasets in order to study the human brain.
+        Prior to joining the lab, I completed a Bachelor&apos;s of Engineering
+        from McMaster University.
+      </p>
+      <p>
+        I am always happy to chat about any projects or opportunities - check
+        out my{" "}
         <a
           href="https://cal.com/jasonkai/coffee-chat?duration=30"
           target="_blank"
           rel="noreferrer"
         >
-          here
-        </a>
-        . Alternatively, feel free connect with me by{" "}
+          calendar
+        </a>{" "}
+        to schedule a meeting. Alternatively, feel free connect with me by{" "}
         <a
           href="mailto:tkai@uwo.ca"
           target="_blank"
