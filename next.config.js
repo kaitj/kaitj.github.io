5531c1ae0db30c8f3d68db98ea670170f568b581
Jason Kai <tkai@uwo.ca>
2024-02-09 12:15:06 -0500

    Update export for config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
-  images:{
+  images: {
     loader: "akamai",
     path: "",
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
