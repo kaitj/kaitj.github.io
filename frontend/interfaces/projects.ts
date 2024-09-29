1a3206bd7ead0039a7914b80ba8c9b8c1ccbef90
Jason Kai <tkai@uwo.ca>
2024-09-29 13:56:42 -0400

    Initialize with svelte

--- frontend/interfaces/projects.ts
+++
@@ -1,9 +0,0 @@
-interface ProjectProps {
-    name: string;
-    image: string;
-    description: string;
-    website: string;
-    github: string;
-}
-
-export default ProjectProps
--- frontend/interfaces/publications.ts
+++
@@ -1,9 +0,0 @@
-interface PubProps {
-    year: number;
-    authors: string[];
-    title: string;
-    journal: string;
-    doi: string;
-}
-
-export default PubProps
