8ad11cba2097674918e13dddadd15d3199994f75
Jason Kai <tkai@uwo.ca>
2024-10-12 16:29:10 -0400

    Update homepage

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,15 +1,28 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import { Avatar } from '@skeletonlabs/skeleton';
+</script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
+<div class="container h-screen mx-auto flex justify-center items-center">
+	<div class="flex flex-col items-center justify-center text-center px-4">
+		<Avatar src="/images/headshot.jpg" border="border-4 border-surface-300-600-token" width="w-48"/>
+		<h3 class="mt-4">Post Doctoral Fellow @ Child Mind Institute</h3>
+		<div class="text-justify mt-4 space-y-4">
+			<p>
+				I&apos;m a researcher and software developer with a passion for leveraging diverse
+				technologies to drive advancements in medical research. In my current role, I develop and
+				optimize reproducible infrastructures to assist development and delivery of new products.
+				Additionally, I contribute to open-source projects and regularly co-organize hackathons to
+				foster collaboration.
+			</p>
+			<p>
+				Previously, I earned my PhD in the
+				<a href="https://khanlab.ca" target="_blank" rel="noreferrer" class="underline">
+					Khan Computational Imaging Lab
+				</a>
+				at the University of Western Ontario, where my research involved engineering processing pipelines
+				for reproducible analysis and applying machine learning to large datasets in order to study the
+				human brain. Prior to that, I completed a Bachelor&apos;s of Engineering from McMaster University.
+			</p>
+		</div>
 	</div>
 </div>
