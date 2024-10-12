8a03cd37565a14a157188ea4c975d4fe5ad60e20
Jason Kai <tkai@uwo.ca>
2024-10-12 11:20:35 -0400

    Swap twitter for cal + formatting

--- frontend/src/lib/components/Footer/Footer.svelte
+++ frontend/src/lib/components/Footer/Footer.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import { AppBar, TabAnchor, TabGroup } from '@skeletonlabs/skeleton';
-	import { Github, Linkedin, Mail, Twitter } from 'lucide-svelte';
+	import { Calendar, Github, Linkedin, Mail } from 'lucide-svelte';
 </script>
 
-<AppBar 
+<AppBar
 	background="bg-transparent"
-    gridColumns="grid-cols-1" 
-    shadow="shadow-2xl" 
-    slotDefault="place-self-center"
+	gridColumns="grid-cols-1"
+	shadow="shadow-2xl"
+	slotDefault="place-self-center"
 >
 	<TabGroup
 		justify="justify-center"
@@ -19,7 +19,9 @@
 	>
 		<TabAnchor target="_blank" href="mailto:jason.kai@childmind.org"><Mail /></TabAnchor>
 		<TabAnchor target="_blank" href="https://github.com/kaitj"><Github /></TabAnchor>
-		<TabAnchor target="_blank" href="https://twitter.com/jasonkai"><Twitter /></TabAnchor>
 		<TabAnchor target="_blank" href="https://linkedin.com/in/jasonkai"><Linkedin /></TabAnchor>
+		<TabAnchor target="_blank" href="https://cal.com/jasonkai/coffee-chat?duration=30"
+			><Calendar /></TabAnchor
+		>
 	</TabGroup>
 </AppBar>
