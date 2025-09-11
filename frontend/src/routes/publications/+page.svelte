5f2c69023c23d229c9de47da76e42f7e53d5c974
Jason Kai <21226986+kaitj@users.noreply.github.com>
2025-09-11 09:56:54 -0400

    Switch badge icon to svg
    Initial commit of badge, need to fix dimensions

--- frontend/src/routes/publications/+page.svelte
+++ frontend/src/routes/publications/+page.svelte
@@ -82,7 +82,12 @@
 								{#each pubs.filter((pub) => pub.year === year) as pub}
 									<div class="mt-4 flex gap-2">
 										<span class="flex w-14 flex-none items-center justify-center">
-											<span class="badge bg-primary-500 text-lg">🖺</span>
+											<span class="badge bg-primary-500 text-lg">
+												<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="auto" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+													<path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"></path><path d="M14 2v4a2 2 0 0 0 2 2h4"></path>
+													<path d="M10 9H8"></path><path d="M16 13H8"></path><path d="M16 17H8"></path>
+												</svg>
+											</span>
 										</span>
 										<span class="flex-auto">
 											<dt class="text-xl"><strong>{pub.title}</strong></dt>
